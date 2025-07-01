e5af9fcaed6b639bedeed49f3f63c91802c6ca9b
hadees <hadees621@gmail.com>
2025-07-01 10:30:05 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/leads/test', [LeadController::class, 'index']);
 
-// Route::get('/test-leads', [LeadController::class, 'index']);
-
 // Route::get('/test-redis', function () {
 //     Cache::put('foo', 'bar', 60); // stores 'bar' for 60 minutes
 //     return Cache::get('foo');     // should return 'bar'
