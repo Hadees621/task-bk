e264f2fde4b0e4e516905b18ef1cdbba34434bde
hadees <hadees621@gmail.com>
2025-07-01 15:18:00 +0500

    cleanup -w

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -7,22 +7,17 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         return Customers::cursorPaginate(20);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function search(Request $request)
     {
         $query = $request->input('q');
 
-        if (! $query) {
+        if (!$query) {
             return response()->json(['message' => 'Search query is required'], 400);
         }
 
